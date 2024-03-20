2863ae4f4c1d821e901b89159836da575cc76231
EternalVelard <87513856+EternalVelard@users.noreply.github.com>
2024-03-20 22:51:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line func-names
+(function () {
+  const carouselItems = document.querySelectorAll('.slide');
+  const prevButton = document.querySelector('.slider-prev');
+  const nextButton = document.querySelector('.slider-next');
+  let currentSlide = 0;
+
+  function showSlide(n) {
+    carouselItems[currentSlide].style.display = 'none';
+    currentSlide = (n + carouselItems.length) % carouselItems.length;
+    carouselItems[currentSlide].style.display = 'block';
+  }
+
+  function nextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function prevSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  function updateSlide() {
+    showSlide(currentSlide);
+  }
+  nextButton.addEventListener('click', nextSlide);
+  prevButton.addEventListener('click', prevSlide);
+
+  window.updateSlide = updateSlide;
+
+  showSlide(currentSlide);
+}());
